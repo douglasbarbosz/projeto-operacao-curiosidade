70f3bc36d947856a918b86a2e5d8f45e418787ab
Douglas Barbosa <douglassbarbosaap@gmail.com>
2023-10-11 10:45:23 -0300

    páginas de relatórios e lista de usuários adicionadas
    junto de suas respectivas páginas css

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function carregar() {
     window.location.href = 'home.html'
 }
+
+function listarUsuarios() {
+    window.location.href = 'lista-usuarios.html'
+}
+
+function sair() {
+    window.location.href = 'index.html'
+}
+
+function imprimir() {
+    
+}
