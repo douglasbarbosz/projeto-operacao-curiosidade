8f5708e97f34d9c8b05907c16d6bd036f2a9656d
Douglas Barbosa <douglassbarbosaap@gmail.com>
2023-10-13 11:56:05 -0300

    adicionando uma função ainda não funcional

--- script.js
+++ script.js
@@ -35,6 +35,37 @@ function abrirCadastrar() {
     location.href = 'pagina-cadastro.html'
 }
 
+function gravar() {
+    const nome = document.getElementsById('nome').value
+    const idade = document.getElementsById('idade').value
+    const email = document.getElementsById('email').value
+    const endereco = document.getElementsById('endereco').value
+    const valores = document.getElementsById('valores').value
+    const status = document.getElementsByName('status').checked
+
+    if (nome && idade && email && endereco && valores) {
+        const tabela = document.querySelector('#tabela>tbody')
+
+        const novaLinha = document.createElement('tr')
+        const novoNome = document.createElement('td')
+        const novoEmail = document.createElement('td')
+        const novoStatus = document.createElement('td')
+
+        novoNome.classList.add('nome')
+        novoEmail.classList.add('email')
+        novoStatus.classList.add('status')
+
+        novoNome.text = nome
+        novoNome.text = status
+        novoStatus.text = status ? 'Ativo' : 'Inativo'
+
+        tabela.appendChild(novaLinha)
+        novaLinha.appendChild(novoNome)
+        novaLinha.appendChild(novoEmail)
+        novaLinha.appendChild(novoStatus)
+    }
+}
+
 function imprimir() {
 
 }
