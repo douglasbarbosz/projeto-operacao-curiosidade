f70f716b8cfe8812c9205fec7d909c8c3ae370dd
Douglas Barbosa <douglassbarbosaap@gmail.com>
2023-10-19 09:54:50 -0300

    adicionando cores novas para as telas

--- script.js
+++ script.js
@@ -39,13 +39,27 @@ function logar() {
 }
 
 window.onload = function() {
-    totalCadastros()
+    totalCadastros(), 
+    totalCadastrosUltimoMes(),
+    cadastrosComPendencia()
 }
 
 function totalCadastros() {
     let valor1 = document.querySelector('#caixa1>p')
 
     valor1.style.color = '#415996'
+}
+
+function totalCadastrosUltimoMes() {
+    let valor2 = document.querySelector('#caixa2>p')
+
+    valor2.style.color = '#3F9D2F'
+}
+
+function cadastrosComPendencia() {
+    let valor3 = document.querySelector('#caixa3>p')
+
+    valor3.style.color = '#C15959'
 }
 
 
